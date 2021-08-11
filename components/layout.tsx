21552a687cd17881998fdf27dbec12604796d9e9
Marcus <marcus@MJK-Macbook-Pro-08072021.local>
2021-08-11 08:57:34 +0200

    changed title

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Marcus '
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Amazing title here'
 
 export default function Layout({
   children,
